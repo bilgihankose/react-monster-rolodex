01526ad6f4b6d02a53dfe20369935e7de5deece5
Bilgihan Kose <bilgihankose@gmail.com>
2021-10-10 15:34:13 +0300

    added cardlist

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import CardList from "./components/CardList/CardList";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header"></header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
+  }
+  render() {
+    return (
+      <CardList>
+        {this.state.monsters.map((monster) => (
+          <h1 key={monster.id}>{monster.name}</h1>
+        ))}
+      </CardList>
+    );
+  }
 }
-
 export default App;
